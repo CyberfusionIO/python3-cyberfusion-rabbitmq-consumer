7f71dfd00a0d4065f7b19eef67f017e3686471b5
William Edwards <wedwards@cyberfusion.nl>
2021-03-04 15:08:45 +0100

    Add fx_command exchange, add exceptions

---
+++ src/cyberfusion/RabbitMQConsumer/exceptions/exchange.py
@@ -0,0 +1,7 @@
+"""Exchange related exceptions."""
+
+
+class ExchangeNotEnabled:
+    """Exception to raise if exchange not enabled."""
+
+    pass
---
+++ src/cyberfusion/RabbitMQConsumer/exchanges/fx_command.py
@@ -0,0 +1,87 @@
+"""Methods for exchange."""
+
+import json
+from typing import Optional
+
+import pika
+
+from cyberfusion.ClusterApiCli import ClusterApiRequest
+from cyberfusion.ClusterSupport import ClusterSupport
+from cyberfusion.Common.Command import CommandNonZeroError, CyberfusionCommand
+from cyberfusion.RabbitMQConsumer.exceptions.exchange import ExchangeNotEnabled
+from cyberfusion.RabbitMQConsumer.RabbitMQ import RabbitMQ
+
+
+def handle(
+    rabbitmq: RabbitMQ,
+    channel: pika.adapters.blocking_connection.BlockingChannel,
+    method: pika.spec.Basic.Deliver,
+    properties: pika.spec.BasicProperties,
+    body: str,
+) -> None:
+    """Handle message."""  # noqa: D202
+
+    # Check if exchange enabled
+
+    if not rabbitmq.config["virtual_hosts"][rabbitmq.virtual_host][
+        "exchanges"
+    ][method.exchange]["enabled"]:
+        raise ExchangeNotEnabled
+
+    # Cast body
+
+    json_body = json.loads(body)
+
+    # Set variable
+
+    command = json_body["command"]
+
+    # Get support object
+
+    support = ClusterSupport()
+
+    # Get command object
+
+    command_obj = support.get_commands(id_=json_body["command_id"])[0]
+
+    # Run command
+
+    print(f"Running command: '{command}'")
+
+    output: Optional[CyberfusionCommand] = None
+
+    try:
+        output = CyberfusionCommand(
+            command,
+            uid=json_body["unix_id"],
+            gid=json_body["unix_id"],
+            path=json_body["path"],
+            environment={
+                "PWD": json_body["path"],
+                "HOME": json_body["home"],
+            },
+        )
+    except CommandNonZeroError as e:
+        print(f"Error running command '{command}': {e}")
+
+        command_obj.return_code = e.rc
+        command_obj.standard_out = e.stdout
+    except Exception as e:
+        print(f"Error running command '{command}': {e}")
+
+        command_obj.return_code = None
+        command_obj.standard_out = None
+
+    if output:
+        print(f"Success running command: '{command}'")
+
+        command_obj.return_code = output.rc
+        command_obj.standard_out = output.stdout
+
+    # Update object
+
+    support.execute_api_call(
+        ClusterSupport.ENDPOINT_COMMANDS + "/" + str(json_body["command_id"]),
+        method=ClusterApiRequest.METHOD_PUT,
+        data=command_obj,
+    )
