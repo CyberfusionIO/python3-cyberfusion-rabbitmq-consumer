4afe9cb03bfe40cc453ca8ac61a7b24f46aebc8e
William Edwards <wedwards@cyberfusion.nl>
2021-05-14 22:19:50 +0200

    Do not use command for dx_borg_repository_archives_list, update changelog

--- src/cyberfusion/RabbitMQConsumer/exchanges/dx_borg_repository_archives_list.py
+++ src/cyberfusion/RabbitMQConsumer/exchanges/dx_borg_repository_archives_list.py
@@ -1,10 +1,10 @@
 """Methods for exchange."""
 
-from typing import Optional
+import json
 
 import pika
 
-from cyberfusion.Common.Command import CommandNonZeroError, CyberfusionCommand
+from cyberfusion.BorgSupport.repositories import Repository
 from cyberfusion.RabbitMQConsumer.RabbitMQ import RabbitMQ
 
 
@@ -17,50 +17,42 @@ def handle(
 ) -> None:
     """Handle message."""  # noqa: D202
 
-    # Get command
+    # Set variables
 
-    command = (
-        rabbitmq.config["virtual_hosts"][rabbitmq.virtual_host_name][
-            "exchanges"
-        ][method.exchange]["command"]
-        + " --repository-path="
-        + json_body["path"]
-        + " --repository-uid="
-        + str(json_body["unix_id"])
-        + " --repository-gid="
-        + str(json_body["unix_id"])
-        + " --identity-file-path="
-        + json_body["identity_file_path"]
-    )
+    passphrase = json_body["passphrase"]
+    path = json_body["path"]
+    identity_file_path = json_body["identity_file_path"]
+    uid = json_body["unix_id"]
+    gid = json_body["unix_id"]
 
-    print(f"Running command: '{command}'")
+    # Get object
 
-    # Run command
+    repository = Repository(path, passphrase, uid, gid, identity_file_path)
 
-    output: Optional[CyberfusionCommand] = None
+    # Get archives
+
+    print(f"Getting archives for Borg repository with path '{path}'")
 
     try:
-        output = CyberfusionCommand(
-            command,
-            uid=json_body["unix_id"],
-            gid=json_body["unix_id"],
-            environment={"BORG_PASSPHRASE": json_body["passphrase"]},
+        archives = repository.list()
+
+        print(
+            f"Success getting archives for Borg repository with path '{path}'"
         )
-    except CommandNonZeroError as e:
-        # If command fails, don't crash entire program
+    except Exception as e:
+        # If action fails, don't crash entire program
 
-        print(f"Error running command '{command}': {e}")
-
-    if output:
-        print(f"Success running command: '{command}'")
-
-        # Publish message
-
-        channel.basic_publish(
-            exchange=method.exchange,
-            routing_key=properties.reply_to,
-            properties=pika.BasicProperties(
-                correlation_id=properties.correlation_id,
-            ),
-            body=output.stdout.encode("utf-8"),
+        print(
+            f"Error getting archives for Borg repository with path '{path}': {e}"  # noqa: E501
         )
+
+    # Publish message
+
+    channel.basic_publish(
+        exchange=method.exchange,
+        routing_key=properties.reply_to,
+        properties=pika.BasicProperties(
+            correlation_id=properties.correlation_id,
+        ),
+        body=json.dumps(archives),
+    )
