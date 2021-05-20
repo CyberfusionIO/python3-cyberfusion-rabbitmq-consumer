9b1e40c6a5b45b1780286cc44d58c20066ce0f84
William Edwards <wedwards@cyberfusion.nl>
2021-05-20 09:49:18 +0200

    Use ClusterSupport to retrieve virtual host information in dx_wordpress_installation_one_time_login_url

--- src/cyberfusion/RabbitMQConsumer/exchanges/dx_wordpress_installation_one_time_login_url.py
+++ src/cyberfusion/RabbitMQConsumer/exchanges/dx_wordpress_installation_one_time_login_url.py
@@ -4,6 +4,7 @@ import json
 
 import pika
 
+from cyberfusion.ClusterSupport import ClusterSupport
 from cyberfusion.RabbitMQConsumer.RabbitMQ import RabbitMQ
 from cyberfusion.WordPressSupport import Installation
 from cyberfusion.WordPressSupport.users import User, Users
@@ -18,16 +19,22 @@ def handle(
 ) -> None:
     """Handle message."""  # noqa: D202
 
-    # Set variables
+    # Get support object
 
-    path = json_body["path"]
-    home = json_body["home"]
-    uid = json_body["unix_id"]
-    gid = json_body["unix_id"]
+    support = ClusterSupport()
 
-    # Get object
+    # Get API object
 
-    installation = Installation(path, uid, gid, home)
+    obj = support.get_virtual_hosts(id_=json_body["virtual_host_id"])[0]
+
+    # Get Installation object
+
+    installation = Installation(
+        obj.public_root,
+        obj.unix_user.unix_id,
+        obj.unix_user.unix_id,
+        obj.unix_user.virtual_hosts_directory,
+    )
 
     # Get administrator users
 
@@ -40,20 +47,20 @@ def handle(
     # Get one time login URL
 
     print(
-        f"Getting one time login URL for installation with path '{path}', user with ID '{user.id}'"  # noqa: E501
+        f"Getting one time login URL for CMS on Virtual Host with public root '{obj.public_root}', user with ID '{user.id}'"  # noqa: E501
     )
 
     try:
         one_time_login_url = user.get_one_time_login_url()
 
         print(
-            f"Success getting one time login URL for installation with path '{path}', user with ID '{user.id}'"  # noqa: E501
+            f"Success getting one time login URL for CMS on Virtual Host with public root '{obj.public_root}', user with ID '{user.id}'"  # noqa: E501
         )
     except Exception as e:
         # If action fails, don't crash entire program
 
         print(
-            f"Error getting one time login URL for installation with path '{path}', user with ID '{user.id}': {e}"  # noqa: E501
+            f"Error getting one time login URL for CMS on Virtual Host with public root '{obj.public_root}', user with ID '{user.id}': {e}"  # noqa: E501
         )
 
     # Publish message
