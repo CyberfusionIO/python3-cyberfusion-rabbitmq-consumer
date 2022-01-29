9dfe12f2a97f7f192660a6c4ba044b34af6c9fa5
William Edwards <wedwards@cyberfusion.nl>
2022-01-29 19:34:11 +0100

    Add dx_service_reload

---
+++ src/cyberfusion/RabbitMQConsumer/exchanges/dx_service_reload.py
@@ -0,0 +1,58 @@
+"""Methods for exchange."""
+
+import logging
+from random import randint
+from time import sleep
+
+import pika
+
+from cyberfusion.Common.Systemd import CyberfusionUnit
+from cyberfusion.RabbitMQConsumer.RabbitMQ import RabbitMQ
+
+logger = logging.getLogger(__name__)
+
+
+def handle(
+    rabbitmq: RabbitMQ,
+    channel: pika.adapters.blocking_connection.BlockingChannel,
+    method: pika.spec.Basic.Deliver,
+    properties: pika.spec.BasicProperties,
+    json_body: dict,
+) -> None:
+    """Handle message."""
+
+    # Set variables
+
+    unit_name = json_body["unit_name"]
+
+    # Get object
+
+    unit = CyberfusionUnit(unit_name)
+
+    # Set delay to minimise the chance of reloading services
+    # providing the same service on multiple nodes at the same time
+
+    delay = randint(5, 10)  # noqa: S311
+
+    # Reload unit
+
+    logger.info(
+        f"Reloading service with unit name '{unit_name}'. Random delay: '{delay}'"
+    )
+
+    # Delay
+
+    sleep(delay)
+
+    try:
+        unit.reload()
+
+        logger.info(f"Success reloading service with unit name '{unit_name}'")
+    except Exception:
+        # If action fails, don't crash entire program
+
+        logger.exception(
+            f"Error reloading service with unit name '{unit_name}'"
+        )
+
+        return
