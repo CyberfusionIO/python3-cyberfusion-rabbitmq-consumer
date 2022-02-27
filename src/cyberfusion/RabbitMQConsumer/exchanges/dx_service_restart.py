7c2e10f823cf44b3637c5d28c628a28d9a6f1123
William Edwards <wedwards@cyberfusion.nl>
2022-02-27 16:15:37 +0100

    Turn dx_service_restart into RPC call

--- src/cyberfusion/RabbitMQConsumer/exchanges/dx_service_restart.py
+++ src/cyberfusion/RabbitMQConsumer/exchanges/dx_service_restart.py
@@ -1,13 +1,16 @@
 """Methods for exchange."""
 
+import json
 import logging
-from random import randint
-from time import sleep
 
 import pika
 
 from cyberfusion.Common.Systemd import CyberfusionUnit
+from cyberfusion.RabbitMQConsumer.exceptions.dx_service_restart import (
+    ServiceRestartError,
+)
 from cyberfusion.RabbitMQConsumer.RabbitMQ import RabbitMQ
+from cyberfusion.RabbitMQConsumer.utilities import _prefix_message
 
 logger = logging.getLogger(__name__)
 
@@ -20,39 +23,45 @@ def handle(
     json_body: dict,
 ) -> None:
     """Handle message."""
-
-    # Set variables
-
-    unit_name = json_body["unit_name"]
-
-    # Get object
-
-    unit = CyberfusionUnit(unit_name)
-
-    # Set delay to minimise the chance of restarting services
-    # providing the same service on multiple nodes at the same time
-
-    delay = randint(5, 10)  # noqa: S311
-
-    # Restart unit
-
-    logger.info(
-        f"Restarting service with unit name '{unit_name}'. Random delay: '{delay}'"
-    )
-
-    # Delay
-
-    sleep(delay)
-
     try:
-        unit.restart()
+        # Set variables
 
-        logger.info(f"Success restarting service with unit name '{unit_name}'")
-    except Exception:
-        # If action fails, don't crash entire program
+        unit_name = json_body["unit_name"]
 
-        logger.exception(
-            f"Error restarting service with unit name '{unit_name}'"
-        )
+        # Set preliminary result
 
-        return
+        success = True
+        result = _prefix_message(unit_name, "Service restarted")
+
+        # Get object
+
+        unit = CyberfusionUnit(unit_name)
+
+        # Restart unit
+
+        logger.info(_prefix_message(unit_name, "Restarting service"))
+
+        try:
+            unit.restart()
+        except Exception:
+            raise ServiceRestartError
+
+    except ServiceRestartError as e:
+        # Set result from error and log exception
+
+        success = False
+        result = _prefix_message(unit_name, e.result)
+
+        logger.exception(result)
+
+    # Publish result
+
+    channel.basic_publish(
+        exchange=method.exchange,
+        routing_key=properties.reply_to,
+        properties=pika.BasicProperties(
+            correlation_id=properties.correlation_id,
+            content_type="application/json",
+        ),
+        body=json.dumps({"success": success, "message": result}),
+    )
