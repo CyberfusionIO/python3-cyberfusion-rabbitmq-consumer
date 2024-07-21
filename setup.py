d9fe18a9b4c6e042608adcad0838d61eb8f033ff
William Edwards <wedwards@cyberfusion.nl>
2024-07-21 11:25:52 +0200

    Update changelog, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python3-cyberfusion-cluster-rabbitmq-consumer",
-    version="2.0.1",
+    version="2.0.2",
     description="RabbitMQ consumer for clusters.",
     author="William Edwards",
     author_email="wedwards@cyberfusion.nl",
