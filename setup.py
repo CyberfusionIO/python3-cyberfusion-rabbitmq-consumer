9f670b2359ee828cd99377394b7ee8d1cf5c8c36
William Edwards <wedwards@cyberfusion.nl>
2024-08-11 15:31:37 +0200

    Update description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="python3-cyberfusion-rabbitmq-consumer",
     version="2.0.2.3",
-    description="RabbitMQ consumer for clusters.",
+    description="RabbitMQ consumer for RPC.",
     author="William Edwards",
     author_email="wedwards@cyberfusion.nl",
     url="https://vcs.cyberfusion.nl/core/python3-cyberfusion-rabbitmq-consumer",
