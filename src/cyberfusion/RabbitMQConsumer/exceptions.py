c3a68a77463d8c9559d2dbf0c4e767ac57839a39
William Edwards <wedwards@cyberfusion.nl>
2024-08-19 12:21:46 +0200

    Don't use blanket exceptions

--- src/cyberfusion/RabbitMQConsumer/exceptions.py
+++ src/cyberfusion/RabbitMQConsumer/exceptions.py
@@ -5,3 +5,9 @@ class VirtualHostNotExistsError(Exception):
     """Virtual host doesn't exist."""
 
     pass
+
+
+class FernetKeyMissingError(Exception):
+    """Fernet key is missing when a Fernet-encrypted message was received."""
+
+    pass
